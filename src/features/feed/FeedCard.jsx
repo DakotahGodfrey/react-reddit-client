76a99c77ef29e47cd5a792ecf559b71fd42e9ca2
Dakotah_Godfrey <dakotah.godfrey.dev@gmail.com>
2021-05-13 16:22:22 -0400

    FeedCard pseudo code added

---
+++ src/features/feed/FeedCard.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const FeedCard = () => {
+  return (
+    <div>
+      {/* Vote Component Here */}
+      {/* Post Information Here */}
+      {/* Post Content Here */}
+      {/* Post Footer Here */}
+    </div>
+  );
+};
+
+export default FeedCard;
+{
+  /* 
+        This card will contain all of the posts components.
+        the post has 4 main components:
+        1) the vote card.
+        2) the post info: title, user, etc...
+        3) the post content.
+        4) a footer with links 
+        I think a 2 column, 3 row layout makes sense for the card itself. 
+    */
+}
