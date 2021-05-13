526e394a11f7013d378a8c36e33517c33302e1d3
Dakotah_Godfrey <dakotah.godfrey.dev@gmail.com>
2021-05-13 15:02:17 -0400

    added pseudo for feed section

---
+++ src/features/feed/Feed.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Feed = () => {
+  return (
+    <section>
+      {/* Feed Navigation Component */}
+      {/* Map r/Popular to Feed Card Components */}
+    </section>
+  );
+};
+
+export default Feed;
