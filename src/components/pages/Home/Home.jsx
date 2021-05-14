80634ff251114ba51e66cf6100d11b7a37b73a02
Dakotah_Godfrey <dakotah.godfrey.dev@gmail.com>
2021-05-13 23:41:21 -0400

    added initial styles to trending aside component

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -5,6 +5,7 @@ import Trending from "../../../features/trending_row/Trending";
 import Navbar from "../../layout/Navbar/Navbar";
 
 const Home = () => {
+  const trendingSubs = ["r/eyebleach", "r/aww", "r/news", "r/doge", "r/stonks"];
   return (
     <main className="home">
       <Navbar />
@@ -16,7 +17,7 @@ const Home = () => {
           {/* FeedContainer and Sub Components */}
           <Feed />
           {/* Sidebar and Sidebar cards */}
-          <Aside />
+          <Aside trendingSubs={trendingSubs} />
         </div>
       </section>
     </main>
