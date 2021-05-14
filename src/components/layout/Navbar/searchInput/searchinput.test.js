a8d364e4eb0849c32222ec0d788174e023133bf7
Dakotah_Godfrey <dakotah.godfrey.dev@gmail.com>
2021-05-14 14:49:44 -0400

    inital searchInput unit tests

---
+++ src/components/layout/Navbar/searchInput/searchinput.test.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import SearchInput from "./SearchInput";
+
+describe("SearchInput", () => {
+  it("should render a form", () => {
+    render(<SearchInput />);
+    expect(screen.getByRole("form")).toBeInTheDocument();
+  });
+  it("should render a search icon", () => {
+    render(<SearchInput />);
+    const searchIcon = screen.getByRole("icon");
+
+    expect(searchIcon).toBeInTheDocument();
+  });
+  it("should render a search input", () => {
+    render(<SearchInput />);
+    const searchField = screen.getByRole("searchbox");
+    expect(searchField).toHaveAttribute("type", "search");
+  });
+});
