4ca5eb070654cc8e6ad55c243195fa3990eed8f6
Dakotah_Godfrey <dakotah.godfrey.dev@gmail.com>
2021-05-14 21:54:32 -0400

    removed harded coded posts

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -5,39 +5,7 @@ import Trending from "../../../features/Trending/Trending";
 import Navbar from "../../layout/Navbar/Navbar";
 
 const Home = () => {
-  const posts = [
-    {
-      title:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, nam?",
-      image: "https://i.redd.it/043rme7pzvy61.jpg",
-      author: "u/throwaway",
-      subreddit: "r/rarepuppers",
-      commentCount: 9,
-    },
-    {
-      title:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, nam?",
-      video: "https://www.youtube.com/embed/G52dUQLxPzg",
-      author: "u/throwaway235234",
-      subreddit: "r/rarepuppers",
-      commentCount: 1233,
-    },
-    {
-      title:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, nam?",
-      image: "https://i.redd.it/043rme7pzvy61.jpg",
-      author: "u/throwaway",
-      subreddit: "r/rarepuppers",
-      commentCount: 453,
-    },
-    {
-      title:
-        "God creates dinosaurs. God destroys dinosaurs. God creates Man. Man destroys God. Man creates Dinosaurs",
-      author: "u/jeffGoldblum",
-      subreddit: "r/jeffsum",
-      commentCount: 666,
-    },
-  ];
+  const posts = [];
   const trendingSubs = ["r/eyebleach", "r/aww", "r/news", "r/doge", "r/stonks"];
   return (
     <main className="home">
