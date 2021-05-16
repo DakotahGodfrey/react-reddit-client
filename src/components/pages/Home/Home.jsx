578e36e9cd305c8e0f4f0c7e369f1c5fd3a0a456
Dakotah_Godfrey <dakotah.godfrey.dev@gmail.com>
2021-05-15 23:07:51 -0400

    initial filter by subreddit feature

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -8,15 +8,15 @@ import Doge from "../../../assets/media/dogecoin.jpeg.jpg";
 import { getPopularPosts, getTrending, selectHome } from "./homeSlice";
 import { useDispatch, useSelector } from "react-redux";
 const Home = () => {
+  const home = useSelector(selectHome);
+  const { posts, trendingItems, currentSubreddit, errors } = home;
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getPopularPosts());
     dispatch(getTrending());
   }, [dispatch]);
-  const home = useSelector(selectHome);
-  const { posts } = home;
+
   const trendingSubs = ["r/eyebleach", "r/aww", "r/news", "r/doge", "r/stonks"];
-  const { trendingItems } = home;
   return (
     <main className="home">
       <Navbar />
@@ -25,7 +25,11 @@ const Home = () => {
       </header>
       <section className="feed" data-testid="feed">
         <div className="feed-wrapper" data-testid="feed-wrapper">
-          <Feed posts={posts} />
+          {errors ? (
+            <p className="feed-error">Sorry, something went wrong</p>
+          ) : (
+            <Feed posts={posts} currentSubreddit={currentSubreddit} />
+          )}
           <Aside trendingSubs={trendingSubs} />
         </div>
       </section>
--- src/features/Feed/Feed.jsx
+++ src/features/Feed/Feed.jsx
@@ -1,17 +1,46 @@
 import React from "react";
 import Toolbar from "../../components/layout/Toolbar/Toolbar";
 import FeedCard from "../../components/layout/FeedCard/FeedCard";
+import { useDispatch } from "react-redux";
+import {
+  getPostsBySubreddit,
+  setCurrentSubreddit,
+} from "../../components/pages/Home/homeSlice";
 
-const Feed = ({ posts }) => {
+const Feed = ({ posts, currentSubreddit }) => {
+  const testFn = () => {
+    console.log("this works");
+  };
+  const dispatch = useDispatch();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(getPostsBySubreddit(currentSubreddit));
+  };
+  const handleChange = (e) => {
+    dispatch(setCurrentSubreddit(e.target.value));
+  };
   return (
     <section className="feed-container" data-testid="feed-container">
       {/* Feed Navigation Component */}
       <Toolbar />
-      <h2 className="feed-heading">r/popular</h2>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <label className="feed-heading" htmlFor="subreddit" onClick={testFn}>
+          r/{currentSubreddit}
+        </label>
+        <input
+          type="text"
+          id="search"
+          name="subreddit"
+          id="subreddit"
+          onChange={(e) => handleChange(e)}
+        />
+      </form>
       <div className="posts-container" data-testid="posts-container">
-        {posts.map((post) => (
-          <FeedCard post={post} key={post.data.id} />
-        ))}
+        {posts[0] === "not found" ? (
+          <p>Sorry, no subreddit was found with that name.</p>
+        ) : (
+          posts.map((post) => <FeedCard post={post} key={post.data.id} />)
+        )}
       </div>
     </section>
   );
