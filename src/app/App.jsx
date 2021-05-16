b3a3fe77672621c94941f4850850c727abff7c21
Dakotah_Godfrey <dakotah.godfrey.dev@gmail.com>
2021-05-16 16:33:03 -0400

    added redux-pesist, persisted post state with local storage

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Provider } from "react-redux";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "../components/pages/Home/Home";
 import Post from "../components/pages/Post/Post";
@@ -7,14 +6,12 @@ import { store } from "./store";
 function App() {
   return (
     <div className="App">
-      <Provider store={store}>
-        <Router>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/post" component={Post} />
-          </Switch>
-        </Router>
-      </Provider>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/post" component={Post} />
+        </Switch>
+      </Router>
     </div>
   );
 }
