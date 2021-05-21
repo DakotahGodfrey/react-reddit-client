c8d49a55c318385b8025916c5305a53c18a3af21
Dakotah_Godfrey <dakotah.godfrey.dev@gmail.com>
2021-05-21 17:01:47 -0400

    added modal styling

--- src/components/layout/Modals/SettingsModal/SettingsModal.jsx
+++ src/components/layout/Modals/SettingsModal/SettingsModal.jsx
@@ -13,19 +13,21 @@ const SettingsModal = ({ show, handleClick, handleDarkToggle }) => {
         <div className="modal-body">
           <form onSubmit={(e) => e.preventDefault()}>
             <div className="form-control">
-              <label htmlFor="dark-mode">Toggle Dark Mode</label>
+              <label htmlFor="dark-mode">Dark Mode</label>
               <input
                 type="checkbox"
                 onClick={handleDarkToggle}
                 name="dark-mode"
                 id="dark-mode"
               />
+              <i className="material-icons">dark_mode</i>
             </div>
           </form>
         </div>
         <footer className="modal-footer">
           <button onClick={handleClick} className="modal-close">
             Close
+            <i className="material-icons">close</i>
           </button>
         </footer>
       </div>
