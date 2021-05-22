b71d79b760c26c7a74833a01e1e82fa8b8b99218
Dakotah_Godfrey <dakotah.godfrey.dev@gmail.com>
2021-05-21 23:08:44 -0400

    feed card styling, rouned down comments

--- src/components/layout/Cards/cardComponents/PostFooter/PostFooter.jsx
+++ src/components/layout/Cards/cardComponents/PostFooter/PostFooter.jsx
@@ -7,11 +7,24 @@ const PostFooter = ({
   removeBookmark,
 }) => {
   const { num_comments } = postLinks;
+  const roundComments = (comments) => {
+    let roundedComments;
+    if (comments >= 1000000) {
+      roundedComments = Math.floor(comments / 1000000) + "m";
+    } else if (comments >= 1000) {
+      roundedComments = Math.floor(comments / 1000) + "k";
+    }
+    return roundedComments;
+  };
+  const num_comments_rounded = roundComments(num_comments);
   return (
     <footer data-testid="post-footer" className="post-footer">
       <div to="/" data-testid="comment-link" className="post-links">
         <i className="material-icons">comment</i>
-        <span data-testid="num_comments">{num_comments} </span> Comments
+        <span data-testid="num_comments">
+          {num_comments ? num_comments_rounded : null}{" "}
+        </span>{" "}
+        Comments
       </div>
       <div to="/" data-testid="share-link" className="post-links">
         <i className="material-icons">share</i>
