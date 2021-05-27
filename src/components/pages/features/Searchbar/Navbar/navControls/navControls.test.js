da413e9515bba309e084c8226382bdd1876dd1c8
Dakotah_Godfrey <dakotah.godfrey.dev@gmail.com>
2021-05-26 20:42:13 -0400

    added unit test for navControls

--- src/components/pages/features/Searchbar/Navbar/navControls/navControls.test.js
+++ src/components/pages/features/Searchbar/Navbar/navControls/navControls.test.js
@@ -1,20 +1,33 @@
 import React from "react";
-import { screen, render } from "@testing-library/react";
+import { screen, render, fireEvent } from "@testing-library/react";
 import NavControls from "./NavControls";
-
+import { BrowserRouter } from "react-router-dom";
+const handleClick = jest.fn();
 describe("NavControls", () => {
+  beforeEach(() => {
+    render(
+      <BrowserRouter>
+        <NavControls handleClick={handleClick} />
+      </BrowserRouter>
+    );
+  });
   it("should render a div with the classname nav-controls", () => {
-    render(<NavControls />);
     const navControlBox = screen.getByTestId("nav-control-box");
     expect(navControlBox).toBeInTheDocument();
     expect(navControlBox).toHaveClass("nav-controls");
   });
-  it("should render a settings button, with a settings icon", () => {
-    render(<NavControls />);
+  it("should render a settings button", () => {
     const settingsButton = screen.getByTestId("settings-button");
     expect(settingsButton).toBeInTheDocument();
-    expect(settingsButton).toContainHTML(
-      '<button data-testid="settings-button"><i class="material-icons">settings</i></button>'
+  });
+  it("should call handle click when the settings button is clicked", () => {
+    fireEvent.click(screen.getByTestId("settings-button"));
+    expect(handleClick).toHaveBeenCalled();
+  });
+  it("should render a link to the bookmarks page", () => {
+    expect(screen.getByTestId("bookmark-link")).toHaveAttribute(
+      "href",
+      "/bookmarks"
     );
   });
 });
