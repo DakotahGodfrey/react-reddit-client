04bbc4650388750aa7c3ef71627be32d1528ffb1
Dakotah_Godfrey <dakotah.godfrey.dev@gmail.com>
2021-05-28 14:57:34 -0400

    nav controls test fix

--- src/components/features/Searchbar/Navbar/navControls/navControls.test.js
+++ src/components/features/Searchbar/Navbar/navControls/navControls.test.js
@@ -1,20 +1,39 @@
 import React from "react";
-import { screen, render } from "@testing-library/react";
+import { screen, render, fireEvent } from "@testing-library/react";
 import NavControls from "./NavControls";
+import { BrowserRouter } from "react-router-dom";
 
 describe("NavControls", () => {
-  it("should render a div with the classname nav-controls", () => {
-    render(<NavControls />);
-    const navControlBox = screen.getByTestId("nav-control-box");
-    expect(navControlBox).toBeInTheDocument();
-    expect(navControlBox).toHaveClass("nav-controls");
-  });
-  it("should render a settings button, with a settings icon", () => {
-    render(<NavControls />);
-    const settingsButton = screen.getByTestId("settings-button");
-    expect(settingsButton).toBeInTheDocument();
-    expect(settingsButton).toContainHTML(
-      '<button data-testid="settings-button"><i class="material-icons">settings</i></button>'
+  const handleClick = jest.fn();
+  beforeEach(() => {
+    render(
+      <BrowserRouter>
+        <NavControls handleClick={handleClick} />
+      </BrowserRouter>
     );
+  });
+  it("should render a nav control menu without error", () => {
+    const menu = screen.getByRole("menu");
+    expect(menu).toBeInTheDocument();
+    expect(menu).toHaveClass("nav-controls");
+  });
+  it("should render a settings button ", () => {
+    const settingsButton = screen.getByRole("button");
+    expect(settingsButton).toBeInTheDocument();
+  });
+  it("should call handleClick when the settings button is clicked", () => {
+    const settingsButton = screen.getByRole("button");
+    fireEvent.click(settingsButton);
+    expect(handleClick).toHaveBeenCalledTimes(1);
+  });
+  it("should render a link to the bookmarks page", () => {
+    const link = screen.getByRole("menuitem");
+    expect(link).toBeInTheDocument();
+    expect(link).toHaveAttribute("href", "/bookmarks");
+  });
+  it("should render an bookmarks icon", () => {
+    const bookmarkIcon = screen.getByText("bookmarks");
+    expect(bookmarkIcon).toBeInTheDocument();
+    expect(bookmarkIcon).toHaveClass("material-icons");
   });
 });
