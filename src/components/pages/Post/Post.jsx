f2a357f1da06583ab40f4fb4f01c9851c7855256
Dakotah_Godfrey <dakotah.godfrey.dev@gmail.com>
2021-05-27 22:28:30 -0400

    post fix ?

--- src/components/pages/Post/Post.jsx
+++ src/components/pages/Post/Post.jsx
@@ -14,11 +14,11 @@ import { selectDarkMode } from "../../features/Searchbar/searchbarSlice";
 
 const Post = ({ match }) => {
   const dispatch = useDispatch();
-  const action = { subreddit: match.params.subreddit, id: match.params.id };
   useEffect(() => {
+    const action = { subreddit: match.params.subreddit, id: match.params.id };
     dispatch(getPostById(action));
     dispatch(getSubredditDescription(match.params.subreddit));
-  }, []);
+  }, [dispatch, match.params.subreddit, match.params.id]);
 
   const dark = useSelector(selectDarkMode);
   const postState = useSelector(selectCurrentPost);
