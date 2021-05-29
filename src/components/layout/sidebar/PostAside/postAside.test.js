fe1b155990dcb20e2f5dfab6f2ced99607c107e1
Dakotah Godfrey <dakotahgodfrey@daniels-mbp.lan>
2021-05-29 16:04:18 -0400

    updated postAside unit tests

--- src/components/layout/sidebar/PostAside/postAside.test.js
+++ src/components/layout/sidebar/PostAside/postAside.test.js
@@ -2,61 +2,74 @@ import { render, screen } from "@testing-library/react";
 import React from "react";
 import { Provider } from "react-redux";
 import PostAside from "./PostAside";
-import store, { persistor } from "../../../../app/store";
+import store from "../../../../app/store";
 import { BrowserRouter } from "react-router-dom";
-import { PersistGate } from "redux-persist/integration/react";
 import { roundSubs } from "../../../../assets/helpers/helpers";
-const subredditData = {
+const subredditDescription = {
   display_name_prefixed: "r/test",
   public_description: "testing testing testing",
   display_name: "test",
   subscribers: 3333,
   accounts_active: 2330,
 };
-const roundedSubs = roundSubs(subredditData.subscribers);
-const roundedActive = roundSubs(subredditData.accounts_active);
+const roundedSubs = roundSubs(subredditDescription.subscribers);
+const roundedActive = roundSubs(subredditDescription.accounts_active);
 beforeEach(() => {
   render(
     <Provider store={store}>
       <BrowserRouter>
-        <PersistGate loading={null} persistor={persistor}>
-          <PostAside subredditData={subredditData} />
-        </PersistGate>
+        <PostAside subredditDescription={subredditDescription} />
       </BrowserRouter>
     </Provider>
   );
 });
 
 describe("Post Aside", () => {
-  it("should render an element with testid of sidebar", () => {
-    expect(screen.getByTestId("sidebar")).toBeInTheDocument();
+  it("should render an div with a class of sidebar", () => {
+    expect(
+      screen.getByRole("complementary").closest("div")
+    ).toBeInTheDocument();
+    expect(screen.getByRole("complementary").closest("div")).toHaveClass(
+      "sidebar post-sidebar"
+    );
   });
-  it("should render an aside", () => {
+  it("should render an aside with role complementary", () => {
     expect(screen.getByRole("complementary")).toBeInTheDocument();
   });
-  it("should render an element with testId aside-header", () => {
-    expect(screen.getByTestId("aside-header")).toBeInTheDocument();
+  it("should render a header with class of aside-header", () => {
+    expect(
+      screen.getByText("About Community").closest("header")
+    ).toBeInTheDocument();
+    expect(screen.getByText("About Community").closest("header")).toHaveClass(
+      "aside-header"
+    );
   });
   it("should rendern an element with text content About Community", () => {
     expect(screen.getByText("About Community")).toBeInTheDocument();
   });
   it("should render a link to the subreddit page", () => {
-    expect(screen.getByTestId("subreddit-link")).toHaveAttribute(
-      "href",
-      "/subreddit"
-    );
+    expect(
+      screen.getByText(subredditDescription.display_name_prefixed).closest("a")
+    ).toHaveAttribute("href", `/r/${subredditDescription.display_name}`);
   });
   it("should render a heading with text content display_name_prefixed", () => {
     expect(screen.getByRole("heading")).toHaveTextContent(
-      subredditData.display_name_prefixed
+      subredditDescription.display_name_prefixed
     );
   });
-  it("should render an element with testid of aside-information", () => {
-    expect(screen.getByTestId("aside-information")).toBeInTheDocument();
+  it("should render an element with class of aside-information", () => {
+    expect(
+      screen.getByText(subredditDescription.public_description).closest("div")
+    ).toBeInTheDocument();
+    expect(
+      screen
+        .getByText(subredditDescription.public_description)
+        .closest("div.aside-information")
+    ).toBeInTheDocument();
   });
   it("should render an element with text content equal to public_description", () => {
     expect(
-      screen.getByText(subredditData.public_description)
+      screen.getByText(subredditDescription.public_description)
     ).toBeInTheDocument();
   });
   it("should render an element with text content equal to subscribers rounded down", () => {
